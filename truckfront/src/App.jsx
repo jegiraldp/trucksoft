e6ca9ba4aa8e9eeccb95d61f97d6d7076edb5c40
jegiraldp <jegiraldp@gmail.com>
2023-08-20 12:51:11 -0500

    200823

--- truckfront/src/App.jsx
+++ truckfront/src/App.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import CategoriesPage from './pages/categories/CategoriesPages.jsx'
 import CategoriesForm from './pages/categories/CategoriesForm.jsx'
+import HomeAdmin from './components/HomeAdmin.jsx'
 
 function App() {
   return (
+   <>
+   <HomeAdmin/>
+   <hr />
     <Routes>
       <Route path="/categories" element={<CategoriesPage/>}/>
       <Route path="/categories/new" element={<CategoriesForm/>}/>
     </Routes >
+   </>
   )
 }
 
---
+++ truckfront/src/components/HomeAdmin.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function HomeAdmin() {
+  return (
+    <div>
+      <h3>Home Admin</h3>
+      <ul>
+        <li><Link>Customer List</Link></li>
+        <li><Link>New Customer</Link></li>
+
+        <li><Link>Provider List</Link></li>
+        <li><Link>New Provider</Link></li>
+
+        <li><Link>Employed List</Link></li>
+        <li><Link>New Employed</Link></li>
+      
+        <li><Link to="/categories">Category List</Link></li>
+        <li><Link to="/categories/new">New Category</Link></li>
+      
+        <li><Link>Element List</Link></li>
+        <li><Link>New Element</Link></li>
+      
+        <li><Link>Service List</Link></li>
+        <li><Link>New Service</Link></li>
+      </ul>
+    </div>
+  );
+}
+
+export default HomeAdmin;
